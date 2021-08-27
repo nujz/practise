fdc9e40b46be528d5940e585768b768dda11d7bb
nujz <nujz@foxmail.com>
2021-08-27 14:16:51 +0800

    101

--- leetcode/101.symmetric-tree/main.go
+++ leetcode/101.symmetric-tree/main.go
@@ -17,6 +17,31 @@ func isSymmetric(root *TreeNode) bool {
 	return check(root, root)
 }
 
+func isSymmetric2(root *TreeNode) bool {
+	l, r := root, root
+	var arr []*TreeNode
+	arr = append(arr, l)
+	arr = append(arr, r)
+	for len(arr) > 0 {
+		l, r := arr[0], arr[1]
+		arr = arr[2:]
+		if l == nil && r == nil {
+			continue
+		}
+		if l == nil || r == nil {
+			return false
+		}
+		if l.Val != r.Val {
+			return false
+		}
+		arr = append(arr, l.Left)
+		arr = append(arr, r.Right)
+		arr = append(arr, l.Right)
+		arr = append(arr, r.Left)
+	}
+	return true
+}
+
 func main() {
 	isSymmetric(nil)
 }
